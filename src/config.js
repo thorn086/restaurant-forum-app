6a8c5599789500372f904ab5bd778baa1341f1db
Darren Rose <thorn086@gmail.com>
2020-03-17 15:06:56 -0500

    changed origin

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default{
-    API_ENDPOINT: 'http://localhost:8000/api',
+    //API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: process.env.API_TOKEN || 'Password@011',
     USER_ID: 'drose@gmail.com',
     API_KEY: process.env.REACT_APP_API_KEY,
     USER_INFO: process.env.USER_INFO,
-    //API_ENDPOINT: 'https://evening-scrubland-37192.herokuapp.com/api'
+    API_ENDPOINT: 'https://evening-scrubland-37192.herokuapp.com/api'
 }
